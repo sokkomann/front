308e26c705f739e70febc3bc7406738a16b6c3bc
kmj <sokkomann@gmail.com>
2026-01-20 11:14:45 +0900

    list-js

--- static/js/market-list.js
+++ static/js/market-list.js
@@ -3,8 +3,6 @@ const tabButtons = document.querySelectorAll(".tab-button");
 const filterBox = document.querySelector(".filter-combobox");
 const orderbyBox = document.querySelector(".orderby-box");
 const heartButtons = document.querySelectorAll(".like-button");
-const modal = document.querySelector(".like-modal-content");
-const likes = heartButtons.firstElementChild;
 
 // 1. 펼치고 닫고, 
 // a를 펼치려고 클릭했을때, b가 펼쳐져있는걸 닫아버림
@@ -36,23 +34,41 @@ document.addEventListener("click", (e) => {
     }
 });
 
-// 4. 찜버튼 클릭
+// 4. 찜버튼 눌렀을때, 개별로 색상변화
+// 나중에 서버연결할때 찜목록 테이블 디비 연결 어쩌고..
 heartButtons.forEach((heart) => {
     heart.addEventListener("click", (e) => {
-        likes.forEach((like) => {
-            like.style.color = rgb(248, 100, 83);
-        });
+        e.preventDefault();
+        
+        if(heart.classList.contains("liked")) {
+            heart.classList.remove("liked");
+            showModal("찜 목록에서 제거 되었습니다.");
+        } else {
+            heart.classList.add("liked");
+            showModal("찜 목록에 추가 되었습니다.");
+        }
     });
 });
 
-let modalCheck;
+// // 4. 찜버튼 눌렀을때, 개별로 눌리고말고에 따라 모달창 메세지 달라지게 출력
+// heartButtons.forEach((heart) => {
+//     heart.addEventListener("click", (e) => {
 
-const showWarnModal = (modalMessage) => {
-    modalCheck = false;
+//         let isCheck = heart.classList.contains("liked");
+//         isCheck ? showModal("찜 목록에서 제거 되었습니다.") : showModal("찜 목록에 추가 되었습니다.");
+//         isCheck = !isCheck;
+//     });
+// });
+
+const showModal = (modalMessage) => {
     document.getElementById("content-wrap").innerHTML = modalMessage;
-    document.querySelector("div.warn-modal").style.animation = "popUp 0.5s";
+    document.querySelector("div.like-modal").style.animation = "popUp 0.5s";
     document.querySelector("div.modal").style.display = "flex";
     setTimeout(() => {
-        modalCheck = true;
-    }, 500);
+        document.querySelector("div.like-modal").style.animation =
+            "popDown 0.5s";
+        setTimeout(() => {
+            document.querySelector("div.modal").style.display = "none";
+        }, 450);
+    }, 1000);
 };
