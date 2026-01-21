afd11ef2b5e8db1144c62d5dd9cc9f2a83b8a504
kmj <sokkomann@gmail.com>
2026-01-21 23:45:28 +0900

    store-detail 대시보드 작업중

--- static/js/store-list.js
+++ static/js/store-list.js
@@ -1,13 +1,19 @@
 const regionBoxes = document.querySelectorAll(".category-each-box");
-const tabButtons = document.querySelectorAll(".tab-button");
-const filterBox = document.querySelector(".filter-combobox");
+
 const orderbyBox = document.querySelector(".orderby-box");
+
+const ddFilter = document.querySelector(".dropdown-filter");
+
 const heartButtons = document.querySelectorAll(".like-button");
 
 // 1. 펼치고 닫고, 
 // a를 펼치려고 클릭했을때, b가 펼쳐져있는걸 닫아버림
 regionBoxes.forEach((regionBox) => {
     regionBox.addEventListener("click", (e) => {
+        if(e.target.closest(".tab-button")) {
+            e.stopPropagation(); // 자식과 부모에 둘다 이벤트가 주어졌을때, 자식을 눌렀을때, 부모의 이벤트가 발생하는걸 방지
+            return;
+        }
         let condition = regionBox.classList.contains("clicked");
         regionBoxes.forEach((box) => {
             box.classList.remove("clicked");
@@ -18,23 +24,39 @@ regionBoxes.forEach((regionBox) => {
 
 // 2. 정렬 버튼 이벤트
 orderbyBox.addEventListener("click", (e) => {
-    let condition = filterBox.classList.contains("clicked");
-    filterBox.classList.toggle("clicked", !condition);
+    let condition = orderbyBox.classList.contains("clicked");
+    orderbyBox.classList.toggle("clicked", !condition);
 });
 
-// 3. 바깥쪽 클릭했을때 펼쳐진거 닫는 이벤트
+// 3. 필터 버튼 이벤트 
+ddFilter.addEventListener("click", (e) => {
+    let condition = ddFilter.classList.contains("clicked");
+    ddFilter.classList.toggle("clicked", !condition);
+});
+
+// 4. 바깥쪽 클릭했을때 펼쳐진거 닫는 이벤트
 document.addEventListener("click", (e) => {
     if(!e.target.closest(".tab-button") && !e.target.closest(".category-each-box")) {
         regionBoxes.forEach((regionBox) => {
             regionBox.classList.remove("clicked");
         });
     }
-    if(!e.target.closest(".filter-combobox") && !e.target.closest(".count-orderby-button-box")) {
-        filterBox.classList.remove("clicked");
+    if(!e.target.closest(".filter-combobox") && !e.target.closest(".orderby-box")) {
+        orderbyBox.classList.remove("clicked");
+    }
+    if(!e.target.closest(".dd-filter-box") && !e.target.closest(".dropdown-filter")) {
+        ddFilter.classList.remove("clicked");
     }
 });
 
-// 4. 찜버튼 눌렀을때, 개별로 색상변화
+// 4-2. 스크롤 움직일때 펼쳐진거 닫는 이벤트
+document.addEventListener("scroll", () => {
+    regionBoxes.forEach((box) => box.classList.remove("clicked"));
+    orderbyBox.classList.remove("clicked");
+    ddFilter.classList.remove("clicked");
+});
+
+// 5. 찜버튼 눌렀을때, 개별로 색상변화
 // 나중에 서버연결할때 찜목록 테이블 디비 연결 어쩌고..
 heartButtons.forEach((heart) => {
     heart.addEventListener("click", (e) => {
@@ -49,17 +71,7 @@ heartButtons.forEach((heart) => {
         }
     });
 });
-
-// // 4. 찜버튼 눌렀을때, 개별로 눌리고말고에 따라 모달창 메세지 달라지게 출력
-// heartButtons.forEach((heart) => {
-//     heart.addEventListener("click", (e) => {
-
-//         let isCheck = heart.classList.contains("liked");
-//         isCheck ? showModal("찜 목록에서 제거 되었습니다.") : showModal("찜 목록에 추가 되었습니다.");
-//         isCheck = !isCheck;
-//     });
-// });
-
+// 모달함수임!
 const showModal = (modalMessage) => {
     document.getElementById("content-wrap").innerHTML = modalMessage;
     document.querySelector("div.like-modal").style.animation = "popUp 0.5s";
