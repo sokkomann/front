8e5f25a0398ed4d6f271838f4aa8f4178cf04636
ljs528022 <ljs528022@gmail.com>
2026-01-23 17:11:15 +0900

    찜한 상품 완성/ 결제 중, 결제완료 html 완성

--- static/js/mypage..js
+++ static/js/mypage..js
@@ -1,8 +1,39 @@
-// 메뉴바 밑의 색상선 움직이게 하기.
+// 메뉴바 페이지네이션 기능
 document.addEventListener("DOMContentLoaded", (e) => {
     const menus = document.querySelectorAll(".Navigation-Span");
     const underbar = document.querySelector(".menu-bar");
     const currnetSpan = document.querySelector(".Navigation-Span.current");
+
+    // 각 페이지의 div 들
+    const profileDiv = document.getElementById("profile");
+    const reviewDiv = document.getElementById("reviews");
+    const likeItemDiv = document.getElementById("likeItems");
+    const pendingItemDiv = document.getElementById("pending");
+    const completeItemDiv = document.getElementById("complete");
+    // 빈 컨탠츠 div
+    const emptyContentDiv = document.getElementById("empty");
+
+    // 후기 페이지 관련
+    const reviewPagination = document.querySelector(".ReviewItem-Pagination");
+    const reviewLinkButton = document.querySelector(".ReviewItem-LinkWrapper");
+    const reviewCurrentPage = document.querySelectorAll(".Pagination-Current");
+
+    const allSection = [
+        profileDiv,
+        reviewDiv,
+        likeItemDiv,
+        pendingItemDiv,
+        completeItemDiv,
+        reviewPagination,
+        reviewLinkButton,
+        emptyContentDiv,
+    ];
+
+    // 받아온 리뷰 값
+    let reviews = true;
+    let likeItems = true;
+    let pendingItems = true;
+    let completeItems = true;
 
     const moveUnderBar = (element) => {
         const left = element.offsetLeft;
@@ -15,9 +46,51 @@ document.addEventListener("DOMContentLoaded", (e) => {
         moveUnderBar(currnetSpan);
     }
 
-    menus.forEach((menu) => {
+    // 메뉴를 누를 시, 해당 페이지를 표시.
+    menus.forEach((menu, i) => {
         menu.addEventListener("click", (e) => {
             e.preventDefault();
+
+            // 각 페이지 누를 때마다 표시되는 div 변경
+            let spanName = menu.getAttribute("name");
+            allSection.forEach((section) => section.classList.add("off"));
+
+            switch (spanName) {
+                case "profile":
+                    profileDiv.classList.remove("off");
+                    reviewDiv.classList.remove("off");
+                    reviewLinkButton.classList.remove("off");
+                    break;
+                case "review":
+                    if (reviews) {
+                        reviewDiv.classList.remove("off");
+                        reviewPagination.classList.remove("off");
+                    } else {
+                        emptyContentDiv.classList.remove("off");
+                    }
+                    break;
+                case "likeItem":
+                    if (likeItems) {
+                        likeItemDiv.classList.remove("off");
+                    } else {
+                        emptyContentDiv.classList.remove("off");
+                    }
+                    break;
+                case "pending":
+                    if (pendingItems) {
+                        pendingItemDiv.classList.remove("off");
+                    } else {
+                        emptyContentDiv.classList.remove("off");
+                    }
+                    break;
+                case "complete":
+                    if (completeItems) {
+                        completeItemDiv.classList.remove("off");
+                    } else {
+                        emptyContentDiv.classList.remove("off");
+                    }
+                    break;
+            }
 
             menus.forEach((m) => m.classList.remove("current"));
             menu.classList.add("current");
@@ -25,7 +98,66 @@ document.addEventListener("DOMContentLoaded", (e) => {
             moveUnderBar(menu);
         });
     });
+
+    const prevBtn = reviewPagination.firstElementChild;
+    const nextBtn = reviewPagination.lastElementChild;
+
+    // 페이지 버튼을 누르면 해당 페이지로 스타일 변경
+    reviewCurrentPage.forEach((current) => {
+        current.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            reviewCurrentPage.forEach((r) => r.classList.remove("active"));
+            current.classList.add("active");
+        });
+    });
+
+    // 찜한 상품의 찜 버튼 누르면 해제시키기
+    const likeItemList = document.querySelectorAll(
+        ".LikeItems-ItemCardWrapper",
+    );
+    const likeItemButtons = document.querySelectorAll(".like-Button");
+    const modal = document.querySelector(".Modal-Container");
+
+    likeItemButtons.forEach((like, i) => {
+        like.addEventListener("click", (e) => {
+            e.preventDefault();
+            likeItemList[i].remove();
+
+            modal.innerHTML = "해당 상품의 찜을 해제했습니다.";
+            modal.classList.add("show");
+
+            // 2. 3초 후에 서서히 사라지게 하기
+            setTimeout(() => {
+                modal.classList.remove("show");
+            }, 3000); // 3000ms = 3초
+        });
+    });
+
+    reviewLinkButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector('.Navigation-Span[name="review"]')?.click();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+
+    // 로딩되면 프로필을 표시
+    document.querySelector('.Navigation-Span[name="profile"]')?.click();
 });
+
+// 마이페이지 결제 중 상품 결제 취소 모달
+// TODO
+
+// 리뷰 가져와서 뿌리기
+const fetchReivews = (reviews) => {};
+
+// 유저 정보 가져오기
+const getUser = async () => {};
+
+// 해당 유저 리뷰들 가져오기
+const getReivew = async () => {};
 
 // 프로필 편집 버튼 누르면 페이지 이동
 const modifyBtn = document.querySelector(".ModifyBtn-Button");
--- static/js/search.js
+++ static/js/search.js
@@ -1,3 +1,37 @@
+// 페이지가 로딩 될 시, 상품 뿌리기
+document.addEventListener("DOMContentLoaded", (e) => {
+    fetchItems();
+
+    // 찜 버튼
+    const likeButtons = document.querySelectorAll(".like-Button");
+
+    // 찜 버튼 누르기 기능
+    likeButtons.forEach((like) => {
+        like.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            // DB에서 찜 관련 값 받아오기
+            like.classList.toggle("is-active");
+
+            const isLiked = like.classList.contains("is-active");
+
+            // 찜 추가 / 해제 로직
+            if (!isLiked) {
+                like.firstElementChild.style.display = "none";
+                like.lastElementChild.style.display = "block";
+                // 찜 추가 로직 넣어야 함
+                addLike();
+            } else {
+                like.firstElementChild.style.display = "block";
+                like.lastElementChild.style.display = "none";
+
+                // 찜 해제 로직 넣어야 함
+                deleteLike();
+            }
+        });
+    });
+});
+
 // 연관 검색어 버튼 기능
 // 누르면 해당 검색어로 검색
 const searchChips = document.querySelectorAll(".SearchRelation-Chip");
@@ -77,10 +111,7 @@ const likeButtons = document.querySelectorAll(".like-Button");
 // 찜 버튼 누르기 기능
 likeButtons.forEach((like) => {
     like.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        e.currentTarget.classList.toggle("is-active");
-
+        console.log("snffla!");
         // DB에서 찜 관련 값 받아오기
 
         const isLiked = e.currentTarget.classList.contains("is-active");
@@ -110,21 +141,15 @@ document.addEventListener("scroll", (e) => {
     });
 });
 
-// 페이지가 로딩 될 시, 상품 뿌리기
-document.addEventListener("DOMContentLoaded", (e) => {
-    console.log("불로옴!");
-    fetchItems();
-});
-
 // --------------------------------------
 // 상품 뿌리기
-const fetchItems = async () => {
+const fetchItems = () => {
     const itemWrapper = document.querySelector(".SearchResult-Wrapper");
     const itemCount = itemWrapper.firstElementChild.firstElementChild;
     const itemContainer = itemCount.nextElementSibling;
 
     // 상품 받아오는 메소드 사용
-    let items = await getItems();
+    // let items = await getItems();
     // items.forEach((item) => {
     //     itemContainer.innerHTML += itemCardLayout(item);
     // });
@@ -164,7 +189,7 @@ const itemCardLayout = (item) => {
                         <!-- 빈하트 -->
                         <svg class="like-empty" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.463 15.3087L19.9993 15.7933L20.5356 15.3087L22.2643 13.747C22.2643 13.747 22.2643 13.7469 22.2643 13.7469C23.615 12.5269 25.9852 12.7532 27.3097 14.2118L27.3156 14.2182L27.3216 14.2246C28.9912 15.9843 29.0145 19.0158 27.2167 20.9218L19.9995 27.9864L12.7818 20.9218C10.9839 19.0158 11.0075 15.984 12.6769 14.2246L12.6829 14.2182L12.6888 14.2118C14.0133 12.7532 16.3836 12.5269 17.7343 13.7469C17.7343 13.7469 17.7343 13.7469 17.7344 13.747L19.463 15.3087Z" fill="black" fill-opacity="0.25" stroke="white" stroke-width="1.6"></path><g filter="url(#filter0_dd_909_4706012)"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.9992 17.111L17.0789 14.4725C17.0789 14.4725 17.079 14.4726 17.0789 14.4725C16.1903 13.67 14.4337 13.7447 13.4126 14.8691L13.3995 14.8835L13.3861 14.8976C12.0903 16.2633 12.039 18.6949 13.4805 20.2374L19.9994 26.6181L26.518 20.2374C27.9595 18.6949 27.9084 16.2637 26.6123 14.8976L26.5989 14.8835L26.5859 14.8691C25.5648 13.7447 23.8085 13.6699 22.9198 14.4725C22.9198 14.4725 22.9198 14.4724 22.9198 14.4725L19.9992 17.111ZM21.728 13.1533C23.4567 11.5918 26.3291 11.942 27.902 13.6739C29.8776 15.7563 29.8775 19.2796 27.7872 21.4827L19.9995 29.1058L12.2112 21.4827C10.1209 19.2796 10.121 15.7559 12.0965 13.6739C13.6694 11.942 16.5419 11.5917 18.2706 13.1533L19.9993 14.7151L21.728 13.1533Z" fill="white"></path></g><defs><filter id="filter0_dd_909_4706012" x="8.62891" y="10.1571" width="22.7422" height="20.9487" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset></feOffset><feGaussianBlur stdDeviation="1"></feGaussianBlur><feComposite in2="hardAlpha" operator="out"></feComposite><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_909_4706012"></feBlend><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset></feOffset><feGaussianBlur stdDeviation="0.5"></feGaussianBlur><feComposite in2="hardAlpha" operator="out"></feComposite><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"></feColorMatrix><feBlend mode="normal" in2="effect1_dropShadow_909_4706012" result="effect2_dropShadow_909_4706012"></feBlend><feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_909_4706012" result="shape"></feBlend></filter></defs></svg>
                         <!-- 찬 하트 -->
-                        <svg class="like-fill" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_dd_909_4706251)"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.9993 14.7154L18.2706 13.1536C16.5419 11.592 13.6694 11.9423 12.0965 13.6742C10.121 15.7562 10.1209 19.2799 12.2112 21.483L19.9995 29.1061L27.7872 21.483C29.8775 19.2799 29.8776 15.7566 27.902 13.6742C26.3291 11.9423 23.4567 11.5921 21.728 13.1536L19.9993 14.7154Z" fill="#F86453"></path></g><defs><filter id="filter0_dd_909_4706251" x="-2" y="-2" width="44" height="44" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset></feOffset><feGaussianBlur stdDeviation="1"></feGaussianBlur><feComposite in2="hardAlpha" operator="out"></feComposite><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_909_4706251"></feBlend><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset></feOffset><feGaussianBlur stdDeviation="0.5"></feGaussianBlur><feComposite in2="hardAlpha" operator="out"></feComposite><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"></feColorMatrix><feBlend mode="normal" in2="effect1_dropShadow_909_4706251" result="effect2_dropShadow_909_4706251"></feBlend><feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_909_4706251" result="shape"></feBlend></filter></defs></svg>
+                        <svg style="display: none;" class="like-fill" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_dd_909_4706251)"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.9993 14.7154L18.2706 13.1536C16.5419 11.592 13.6694 11.9423 12.0965 13.6742C10.121 15.7562 10.1209 19.2799 12.2112 21.483L19.9995 29.1061L27.7872 21.483C29.8775 19.2799 29.8776 15.7566 27.902 13.6742C26.3291 11.9423 23.4567 11.5921 21.728 13.1536L19.9993 14.7154Z" fill="#F86453"></path></g><defs><filter id="filter0_dd_909_4706251" x="-2" y="-2" width="44" height="44" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset></feOffset><feGaussianBlur stdDeviation="1"></feGaussianBlur><feComposite in2="hardAlpha" operator="out"></feComposite><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_909_4706251"></feBlend><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset></feOffset><feGaussianBlur stdDeviation="0.5"></feGaussianBlur><feComposite in2="hardAlpha" operator="out"></feComposite><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"></feColorMatrix><feBlend mode="normal" in2="effect1_dropShadow_909_4706251" result="effect2_dropShadow_909_4706251"></feBlend><feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_909_4706251" result="shape"></feBlend></filter></defs></svg>
                     </button>
                 </div>
                 <div class="ItemCard-Content">
